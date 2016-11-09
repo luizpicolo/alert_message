22890a43804fbb91f052965e452ec28255180a76
Luiz Picolo <luizpicolo@gmail.com>
2016-11-09 16:47:24 -0300

    Added tests

--- lib/alert_message/engine.rb
+++ lib/alert_message/engine.rb
@@ -3,5 +3,12 @@ module AlertMessage
     initializer 'alert_message.load_static_assets' do |app|
       app.middleware.use ::ActionDispatch::Static, "#{root}/vendor"
     end
+
+    config.generators do |g|
+      g.test_framework :rspec, :fixture => false
+      g.fixture_replacement :factory_girl, :dir => 'spec/factories'
+      g.assets false
+      g.helper false
+    end
   end
 end
