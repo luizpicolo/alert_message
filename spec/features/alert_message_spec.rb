66653d25c0fdba0f9a72c45f6870a5e572dc37f8
Luiz Picolo <luizpicolo@gmail.com>
2016-11-09 16:47:24 -0300

    Added tests

---
+++ spec/features/alert_message_spec.rb
@@ -0,0 +1,14 @@
+require 'spec_helper'
+
+feature 'show message', type: :feature do
+
+  selectors = %w(error notice success)
+
+  selectors.each do |selector|
+    scenario selector, js: false do
+      visit home_path(message: selector)
+      expect(page).to have_selector(".alert-message-#{selector}")
+      expect(page).to have_text("test #{selector} message")
+    end
+  end
+end
